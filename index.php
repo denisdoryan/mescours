3aa7e2848f5614528d9cb7fe55062786c082f29f
Doryan <71377447+denisdoryan@users.noreply.github.com>
2022-01-24 19:12:16 +0100

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+session_start(); 
+if (isset($_SESSION['count']) && ($_SESSION['count'] > 4)) {
+	header('Location: http://www.google.com/');
+} else {
+?> 
 <!DOCTYPE html>
 <html>
 	<head>
@@ -12,6 +18,23 @@
 	</header>
 
 	<body>
+		<?php
+	$password = 'motdepasse';
+	if ((isset($_POST['password']) && ($_POST['password'])) != $password) {
+		if (!isset($_SESSION['count'])) {
+			$_SESSION['count'] = 0;
+		} else {
+			$_SESSION['count']++;
+		}
+	?> 
+<h1>Connexion</h1>
+<form name="form" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>"> 
+<p><label for="password">Mot de passe</label> <input type="password" title="Saisissez le mot de passe" name="password" /></p> 
+<p><input type="submit" name="submit" value="Connexion" /></p> 
+</form>
+	<?php
+	} else { ?> 
+		<p>
 		<p><img src="logoism.png" alt="Logo de l'Institution Sainte-Marie" id="logo" />
 		<p><h2>Liste des matières</h2></p>
 		<p><div id="boutton">
@@ -23,7 +46,11 @@
 				<li><em><a href="Cours SPC/spc.html"><span class="spc">Physique-Chimie</span></a></em></li>
 				<li><em><a href="Cours MATEXP/mathsexp.html"><span class="mathsexp">Mathématiques Expertes</span></a></em></li>
 			</ul></div></p>
-				
+		</p>
+<?php 
+	}
+} 
+?>			
 
 	</body>
 
